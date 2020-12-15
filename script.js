afb4ef73261af4e32ea93c8032a3c6bd51f7aa8e
Hv0stjuk <wot.hvostjuk@gmail.com>
2020-12-15 17:12:43 +0300

    feat: add navigation menu switch & anchor link

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-const hello = 'Hello, world!'
+const elementsNavigationList = document.querySelector('.navigation__list');
+const buttonSliderControllers = document.querySelector('.slider__arrows-controllers');
+
+function switchNavigationItem() {
+  elementsNavigationList.addEventListener ('click', (event)=> {
+    if (event.target.tagName === 'A'){
+      elementsNavigationList.querySelectorAll('.navigation__item').forEach(el => el.classList.remove('active-nav'));
+      event.target.parentElement.classList.add('active-nav');
+    }
+  });
+
+};
+switchNavigationItem()
+
+function switchSliders() {
+buttonSliderControllers.addEventListener('click', (event) => {
+  const sladerImage = document.querySelector('.slider__slide-1');
+  const sliderBackgroundColor = document.querySelector('.slider');
+  sladerImage.classList.toggle('slider__slide-1--hidden');
+  sliderBackgroundColor.classList.toggle('slider--color');
+});
+};
+switchSliders()
+
+window.addEventListener('scroll', (event) => {
+  const navigationItemsArry = elementsNavigationList.querySelectorAll('.navigation__item');
+  if (pageYOffset <=599) {
+    navigationItemsArry.forEach(el => el.classList.remove('active-nav'));
+    navigationItemsArry[0].classList.add('active-nav');
+  } else
+  if (pageYOffset >= 600 & pageYOffset <= 1100) {
+    navigationItemsArry.forEach(el => el.classList.remove('active-nav'));
+    navigationItemsArry[1].classList.add('active-nav');
+  } else 
+  if (pageYOffset >=1101) {
+    navigationItemsArry.forEach(el => el.classList.remove('active-nav'));
+    navigationItemsArry[2].classList.add('active-nav');
+  }
+});
